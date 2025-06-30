4cd1498d7ac08cee1ba5091230d42ea23ce5ada8
Shailesh Bothe <shaileshbothe33@gmail.com>
2025-06-30 14:11:36 +0530

    Fixing Port Problem of express

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
 }));
 
